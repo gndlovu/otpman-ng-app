eb142e677bc91fd00b0c5866678e9297f51b0638
Gladwell Ndlovu <GladwellN@miway.co.za>
2024-09-21 10:05:01 +0200

    Revert to none standalone App

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './shared/guards/auth.guard';
+import { GuestGuard } from './shared/guards/guest.guard';
+import { ErrorComponent } from './pages/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadChildren: () => import('./layout/auth/auth.module').then(m => m.AuthModule), canActivate: [AuthGuard] },
+  { path: '', loadChildren: () => import('./layout/guest/guest.module').then(m => m.GuestModule), canActivate: [GuestGuard] },
+  { path: '**', component: ErrorComponent },
+];
